946483dfb2e6389a7d5fe65b1cd7ce7f4107d5a6
AG Build <build@airgourmet.com>
2026-03-21 19:31:11 +0000

    fix: remove duplicate post method from Notifications extension

--- ios-swift/AirGourmet/Utilities/Helpers/Notifications.swift
+++ ios-swift/AirGourmet/Utilities/Helpers/Notifications.swift
@@ -14,16 +14,3 @@ struct EmailNotifications {
 }
 
 struct EmptyResponse: Codable {}
-
-// Extend APIClient to allow internal post calls
-extension APIClient {
-    func post<Body: Encodable, Response: Decodable>(_ path: String, body: Body, response: Response.Type) async throws -> Response {
-        guard let url = URL(string: "https://api.air-gourmet.com" + path) else { throw APIError.invalidURL }
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = try JSONEncoder().encode(body)
-        let (data, _) = try await URLSession.shared.data(for: request)
-        return try JSONDecoder().decode(Response.self, from: data)
-    }
-}
